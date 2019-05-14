3b5efaf93db98113c52347a1cf630027352732b4
Gerardo Keys <jotojry@icloud.com>
2019-05-13 17:48:02 -0700

    re-factored using fetch api, buttons added

--- api.js
+++ api.js
@@ -1,32 +1,25 @@
-class ImdbGenerator {
-  constructor() {
-    this.$results = $("#results");
-    this.pages = new Pages();
-    this.getId = 
-  }
 
-  createUrl(id) {
-    let url = "http://www.omdbapi.com/?apikey=ba2a0c60&";
-    url += id;
-    return url;
-  }
+const $movie = $('#movie');
+const $results = $("#results");
+const $pagination = $('.pagination');
+const url = "http://www.omdbapi.com/?apikey=ba2a0c60&";
 
-  getReview(res) {
-    $.each(res.Search, (index, movie) => {
-      let search = this.createUrl(`i=${movie.imdbID}`);
-      $.ajax({
-        type: "GET",
-        url: search,
-        dataType: "json",
-        success: (response) => {
-            this.createMovies(response);
-          }
-      }); //end ajax  
-    }); //end loop
-  }
+const getMovies = (url) => {
+  return fetch(url)
+          .then(val => val.json())
+          .catch(err => console.log('There was an error', err))
+}
 
-  createMovies(res) {
-    console.log(res);
+const getIds = (movs) => {
+  return movs.map( mov => mov.imdbID)
+}
+
+const getMoviesbyId = (val) => {
+  return val.map(mov => getMovies(url + `i=${mov}`))
+}
+
+const createMovies = (res) => {
+    //console.log(res);
     if(res['Ratings'].length > 0){
       let construct = '<div class="posterContainer">';
       construct += `<img src="${res['Poster']}" class="poster" >`;
@@ -37,31 +30,30 @@ class ImdbGenerator {
       ${res['Ratings'][1] ? res['Ratings'][1]['Value'] : ''}<br><br>
       ${res['Ratings'][2] ? res['Ratings'][2]['Source'] : ''}<br>
       ${res['Ratings'][2] ? res['Ratings'][2]['Value'] : ''}<br>
-
+  
       </div>`;
       construct += `<span>${res['Title']}</span><br>`;
       construct += '</div>';
-      this.$results.append(construct);
+      $results.append(construct);
     } 
-    
   }
 
-  sendAjax(movie) {
-    $.ajax({
-      type: "GET",
-      url: movie,
-      dataType: "json",
-      success: (response) => {
-          //console.log(response);
-          
-          this.pages.generateButtons(response);
-          this.getReview(response);
+const generateButtons = (results) => {
+    //console.log(results);
+    let res = results.totalResults
+    let num = Math.floor((res / 10));
+    if (res > 100) {
+        for(let i = 1; i <= 10; i ++){
+            $pagination.append(
+                `<button class="button">${i}</button>`
+                );
         }
-    }); //end ajax
-  }
+    } else {
+        for(let i = 1; i <= num; i ++){
+            $pagination.append(
+                `<button class="button">${i}</button>`
+                );
+        }
+    }
 }
-
-
-
-
 
--- main.js
+++ main.js
@@ -1,23 +1,39 @@
-const gen = new ImdbGenerator();
-const $movie = $("#movie");
+//const gen = new ImdbGenerator();
+//const $movie = $("#movie");
 
 $("form").submit(function(event) {
     event.preventDefault();
-    let mov = gen.createUrl(`s=${$movie.val()}`);
-    gen.sendAjax(mov);
-}); //end form
+    const newUrl = url + `s=${$movie.val()}`
+    getMovies(newUrl)
+    .then(val => {
+        generateButtons(val)
+        return val
+    })
+      .then(val => getIds(val.Search)) //gets search results by ID
+      .then(val => getMoviesbyId(val))
+      .then(val => Promise.all(val))
+      .then(val => val.map(mov => {
+        return createMovies(mov);
+      }))
+      
+  
+  }); //end form
 
 $(document).on('mouseenter', '.poster', function () {
-    $(this).prop('style', 'opacity: 0.2');
-    $(this).next().show();
+    $(this)
+        .prop('style', 'opacity: 0.2')
+        .next()
+        .show()
     $(this).on('mouseleave', function () {
-      $(this).prop('style', 'opacity: 1');
-      $(this).next().hide();
+        $(this).prop('style', 'opacity: 1');
+        $(this).next().hide();
     }); //end mouseleave
 }); //end mouseenter
 
 $(document).on('mouseenter', '.cover', function () {
-    $(this).prev().prop('style', 'opacity: 0.2');
+    $(this)
+        .prev()
+        .prop('style', 'opacity: 0.2');
     $(this).show();
 }); //end mouseenter
 
